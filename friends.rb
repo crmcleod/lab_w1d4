ac406495bc5b60198a514d187fd24ca7fff00cd0
Craig McLeod <mcleod.craig@outlook.com>
2020-05-14 15:05:45 +0100

    Exercise 3

--- friends.rb
+++ friends.rb
@@ -1,3 +1,5 @@
+require("pry")
+
 def get_name(person)
   return person[:name]
 end
@@ -5,3 +7,12 @@ end
 def get_favourite_tv_show(person)
   return person[:favourites][:tv_show]
 end
+
+def likes_to_eat(person, food)
+  access_food = person[:favourites][:snacks]
+  binding.pry
+  access_food.include?(food)
+  binding.pry
+end
+
+
